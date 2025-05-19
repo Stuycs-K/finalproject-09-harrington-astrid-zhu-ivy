8fe89ffa1ea0d16d9607c98bace799d2355f5191
ivy zhu <ivyzhu456@gmail.com>
2025-05-19 15:31:24 -0400

    Made right rotate for integers

--- Transform.java
+++ Transform.java
@@ -1,14 +1,14 @@
 class Transform{
   public static void main(String[] args){
     String input = "hello world";
-    byte[] w = new byte[64];
-    // w[1] = 01101111001000000111011101101111;
-    // w[1] = rotate(w[1], 7);
-    // int n = (int) w[1];
-    int n = (int) 01101111001000000111011101101111;
+    byte[] w = input.getBytes();
+    String w1 = "01101111001000000111011101101111";
+    // w[1] = (byte) Integer.parseInt(w1, 2);
+    int n = Integer.parseInt(w1, 2);;
+    System.out.println(Integer.toBinaryString(n));
+    // int n = (int) 01101111001000000111011101101111;
     n = Integer.rotateRight(n, 7);
-    print(w[1]);
-    print((byte) n);
+    System.out.println(Integer.toBinaryString(n));
   }
 
   // public static byte rotate(byte b, int n){
