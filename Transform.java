56e7137fecf899a962fba627818eb911524a72ba
ivy zhu <ivyzhu456@gmail.com>
2025-05-20 14:30:29 -0400

    Step 5 (create message schedule) done

--- Transform.java
+++ Transform.java
@@ -1,7 +1,6 @@
 class Transform{
   public static void main(String[] args){
     String input = "hello world";
-    byte[] w = input.getBytes();
     String w1 = "01101111001000000111011101101111";
     // w[1] = (byte) Integer.parseInt(w1, 2);
     int n = Integer.parseInt(w1, 2);;
@@ -14,9 +13,25 @@ class Transform{
 
     int s0 = Integer.rotateRight(n, 7) ^ Integer.rotateRight(n, 18) ^ rightshift(n, 3);
     System.out.println(Integer.toBinaryString(s0).equals("11001110111000011001010111001011"));
+    int[] w = new int[64];
+    w[0] = Integer.parseInt("01101000011001010110110001101100", 2);
+    w[1] = Integer.parseInt("01101111001000000111011101101111", 2);
+    msgSchedule(w);
   }
 
   public static int rightshift(int n, int d){
     return (n >>> d);
+  }
+
+  public static void msgSchedule(int[] w){
+    for (int i = 16; i < 17; i++){
+      int n = w[i-15];
+      int s0 = Integer.rotateRight(n, 7) ^ Integer.rotateRight(n, 18) ^ rightshift(n, 3);
+      n = w[i-2];
+      int s1 = Integer.rotateRight(n, 17) ^ Integer.rotateRight(n, 19) ^ rightshift(n, 10);
+      w[i] = w[i-16] + s0 + w[i-7] + s1;
+    }
+    System.out.println(w[16]); // mod 2^32 ?
+    System.out.println(Integer.parseInt("00110111010001110000001000110111", 2));
   }
 }
