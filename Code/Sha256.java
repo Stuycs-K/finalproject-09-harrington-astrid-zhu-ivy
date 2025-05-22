dfd2a9323920231ea97bc6bd0e66e5072a817a27
Astrid Harrington <aharrington50@stuy.edu>
2025-05-22 14:50:38 -0400

    started Sha256.java

--- Code/Sha256.java
+++ Code/Sha256.java
@@ -1,5 +1,22 @@
 class Sha256{
+    String message = "";
+    
     public static void main(String[] args){
-        
+        if (parseArgs(args)){
+
+        }
+
+        //PREPROCESSING BEGINS
+    }
+
+    public static boolean parseArgs(String[] args){
+        if (args.length != 1){
+            System.out.println("Please provide only one argument to hash. (java Sha256 message_to_hash)");
+            return(false);
+        }
+        else{
+            message = args[0];
+            return(true);
+        }
     }
 }
